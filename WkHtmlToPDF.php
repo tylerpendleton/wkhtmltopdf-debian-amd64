3701a9e207ca9ab23f35c2d8738982dec6372b0e
Tyler Pendleton <tyler@tylerpendleton.com>
2020-12-20 16:54:25 -0800

    Initial commit

---
+++ WkHtmlToPDF.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace tylerpendleton\WkHtmlToPDF;
+
+/**
+ * Class WkHtmlToPDF
+ * @package tylerpendleton\WkHtmlToPDF
+ */
+class WkHtmlToPDF
+{
+    const PATH = __DIR__ . '/bin/wkhtmltopdf-debian-amd64';
+}
